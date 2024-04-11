11a947a5fa3ee719f1060816a9688eb51aa0c0a9
adamrefaat <159152144+adamrefaat@users.noreply.github.com>
2024-04-12 01:43:44 +0200

    Add files via upload

--- clan.h
+++ clan.h
@@ -1,10 +1,22 @@
 #ifndef CLAN_H
 #define CLAN_H
 
-class Clan
+#include <QFile>
+#include <QTextStream>
+#include <QCoreApplication>
+#include <QPixmap>
+#include <QGraphicsItem>
+#include <QGraphicsTextItem>
+#include <QGraphicsPixmapItem>
+
+class Clan : public QGraphicsPixmapItem
 {
 public:
     Clan();
+    void displayClanDesign();
+
+private:
+    int num;
 };
 
 #endif // CLAN_H
